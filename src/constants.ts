91016f7c644fbc6f2c4e7d7aebb539dbfc193245
Maurolezcano81 <maurolezcano111@gmail.com>
2025-02-16 06:01:29 -0300

    Fix endpoint route

--- src/constants.ts
+++ src/constants.ts
@@ -5,6 +5,6 @@ const baseUrlApi = import.meta.env.VITE_API_URL;
 export const endpointsUrls = {
 
     getEvents: `${baseUrlApi}/eventos`,
-    getEventById: `${baseUrlApi}/eventosa`
+    getEventById: `${baseUrlApi}/eventos`
 
 }
